1db32a12643effb0b53f059a390ed084ef6ba3f2
YongbeenK <52960165+yongbeenK@users.noreply.github.com>
2024-04-27 14:27:28 +0900

    Update buffer_test.sv

--- buffer_test.sv
+++ buffer_test.sv
@@ -4,19 +4,23 @@ class buffer_test extends uvm_test
   
   `uvm_component_utils(buffer_test)
 
-  function new(string name, uvm_component parent null);
-    super.new();
+  function new(string name = "buffer_test", uvm_component parent null);
+    super.new(name, parent);
   endfunction
 
-  buffer_dut dut;
+  buffer_env env;
   
-  function build_phase();
+  virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    
-  endfunction
+    env = buffer_env::type_id::create("env",this);    
+  endfunction: build_phase
 
+  virtual function void start_of_simulation_phase(uvm_phase phase);
+    uvm_root::get().print_topology();
+  endfunction: start_of_simulation_phase
+
+  //?
   virtual task run_phase(uvm_phase phase);
-
     #20
     TBD_intf.a = 1;
     TBD_intf.en = 0;
@@ -34,7 +38,7 @@ class buffer_test extends uvm_test
     TBD_intf.en = 0;
 
     super.run_phase(phase);
-    
   endtask
+  // ?
     
 endclass
